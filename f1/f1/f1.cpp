a3d5a4905ed0314ba9ad9b9e93be23eb71d83aec
Deyi Zou <Dzou1734@conestogac.on.ca>
2024-05-16 14:38:42 -0400

    f1 file created

---
+++ f1/f1/f1.cpp
@@ -0,0 +1,14 @@
+/* ============================================================= */
+/* FILE : f1.cpp                                                 */
+/* PROJECT : Focus Assignment 1                                  */
+/* PROGRAMMER : Deyi Zou                                         */
+/* FIRST VERSION : 2024-05-07                                    */
+/* DESCRIPTION : takes in information about flights and          */
+/*               use dynamic memory to store the destination     */ 
+/*               and date in an array of structs to be displayed */
+/* ============================================================= */
+
+int main(void){
+                               
+    return 0;
+}
